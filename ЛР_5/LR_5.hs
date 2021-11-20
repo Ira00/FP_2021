5dc37361ebd7572f636510bff220d4268e35abe0
Irina <ira.ivahnenko00@gmail.com>
2021-11-20 17:40:14 +0200

    LR5

--- ЛР_5/LR_5.hs
+++ ЛР_5/LR_5.hs
@@ -1,8 +1,10 @@
+
 -- Лабораторна робота №5
 -- студентки групи КН-31 підгрупа 1
 -- Івахненко Ірини
 
--- Мета: Набути досвiду визначення та використання функцiй вищого порядку.
+-- Мета: Ознайомитись з модульною органiзацiєю програм та засобами введення- виведення. Набути досвiду компiляцiї Haskell-програм.
+
 
 -- Варіант 6
  -- cd ЛР_5
@@ -17,73 +19,39 @@ f1a (x:xs)
     | xs == [] = [x]
     | x == head (xs) = f1a xs
     | otherwise = x : f1a xs
--- f1a [1,1,1,5,5,3, 1,1,222,222,222,222]
 
--- result - [1,5,3,1,222]
-
--- б) з застосуванням вбудованих функцiй вищого порядку.
-headmy :: [a] -> a
-headmy (a:_) = a
-headmy [] = error "0"
-
-f1b:: Eq a => [a] -> [a] 
-f1b (x:xs)
-    | xs == [] = [x]
-    | x == headmy (xs) = f1b xs
-    | otherwise = x : f1b xs
--- f1b [1,1,1,5,5,3, 1,1,222,222,222,222]
-
--- result - [1,5,3,1,222]
--- Завдання 2. Знайти найменше спiльне кратне двох чисел.
-
--- а) без застосування
 f2a :: (Integral a) => a -> a -> a
 f2a x y =  (x `div` (gcd x y)) * y
--- f2a 3 19
 
--- result - 57
 
--- б) з застосуванням вбудованих функцiй вищого порядку.
-gcdmy :: Integral b => (b, b) -> b
-gcdmy (x1, 0)  =  x1
-gcdmy (0, x2)  =  x2
-gcdmy (x1, x2) =  gcdmy (x2, (x1 `mod` x2))
-
-f2b :: (Integral a) => a -> a -> a
-f2b x y =  (x `div` (gcdmy (x, y))) * y
--- f2b 3 19
-
--- result - 57
-
--- Висновок: в ході лабораторної роботи набули досвiду визначення та використання функцiй вищого порядку.
-keyf1a :: IO ()
-keyf1a = do
+ -- 5.2.1. Реалiзувати та скомпiлювати одну з програм, розроблених у лабора- торнiй роботi № 3 для Вашого варiанта з введенням даних: 
+ -- а) з клавiатури, б) з файлу та виведенням результатiв: в) на екран, г) у файл.
+keyToConsoleF1 :: IO ()
+keyToConsoleF1 = do
     putStrLn "Enter massive: "
     numb1 <- getLine
     let x = read numb1 :: [Integer]
     z <- return (f1a x)
     putStrLn "Result" 
     print z
--- > keyf1a
+-- > keyToConsoleF1
 -- Enter massive: 
 -- [1,1,1,5,5,3, 1,1,222,222,222,222]
 -- Result
 -- [1,5,3,1,222]
-keyf1b :: IO ()
-keyf1b = do
+keyToFileF1 :: IO ()
+keyToFileF1 = do
     putStrLn "Enter massive: "
     numb1 <- getLine
     let x = read numb1 :: [Integer]
-    z <- return (f1b x)
-    putStrLn "Result" 
-    print z
--- > keyf1b
+    z <- return (f1a x)
+    writeFile "f1_output.txt" ("Result " ++ show z)
+-- > keyToFileF1
 -- Enter massive: 
 -- [1,1,1,5,5,3, 1,1,222,222,222,222]
--- Result
--- [1,5,3,1,222]
-keyf2a :: IO ()
-keyf2a = do
+
+keyToConsoleF2 :: IO ()
+keyToConsoleF2 = do
     putStrLn "Enter first number: "
     numb1 <- getLine
     let x = read numb1 :: Integer
@@ -93,7 +61,7 @@ keyf2a = do
     z <- return (f2a x y)
     putStrLn "Result" 
     print z
--- > keyf2a
+-- > keyToConsoleF2
 -- Enter first number: 
 -- 3
 -- Enter second number: 
@@ -101,24 +69,22 @@ keyf2a = do
 -- Result
 -- 57
 
-keyf2b :: IO ()
-keyf2b = do
+keyToFileF2 :: IO ()
+keyToFileF2 = do
     putStrLn "Enter first number: "
     numb1 <- getLine
     let x = read numb1 :: Integer
     putStrLn "Enter second number: "
     numb2 <- getLine
     let y = read numb2 :: Integer
-    z <- return (f2b x y)
-    putStrLn "Result" 
-    print z
--- > keyf2b
+    z <- return (f2a x y)
+    writeFile "f2_output.txt" ("Result " ++ show z)
+-- > keyToFileF2
 -- Enter first number: 
 -- 3
 -- Enter second number: 
 -- 19
--- Result
--- 57
+
 f :: String -> [Int]
 f = read
 
@@ -128,17 +94,38 @@ ff = read
 fff :: Char -> Int
 fff = read . pure
 
-filef1 :: IO ()
-filef1 = do        
+fileToFileF1 :: IO ()
+fileToFileF1 = do        
         s <- readFile "f1_input.txt"
         let r = f1a (f s)
         writeFile "f1_output.txt" ("Result " ++ show r)
--- > filef1
-filef2 :: IO ()
-filef2 = do        
+-- > fileToFileF1
+fileToConsoleF1 :: IO ()
+fileToConsoleF1 = do        
+        s <- readFile "f1_input.txt"
+        let r = f1a s
+        putStrLn "Result" 
+        print r
+-- > fileToConsoleF1
+-- Result
+-- "[1,1,1,5,5,3, 1,1,2,2,2,2]"
+fileToFileF2 :: IO ()
+fileToFileF2 = do        
         s <- readFile "f2_input.txt"
         let numb1 = fff (head s)
         let numb2 = ff (tail s)
         let r = f2a numb1 numb2
         writeFile "f2_output.txt" ("Result " ++ show r)
--- > filef2
+-- > fileToFileF2
+fileToConsoleF2 :: IO ()
+fileToConsoleF2 = do        
+        s <- readFile "f2_input.txt"
+        let numb1 = fff (head s)
+        let numb2 = ff (tail s)
+        let r = f2a numb1 numb2
+        putStrLn "Result" 
+        print r
+-- > fileToConsoleF2
+-- Result
+-- 57
+-- Висновок: в ході лабораторної роботи ознайомились з модульною органiзацiєю програм та засобами введення- виведення. Набули досвiду компiляцiї Haskell-програм.
